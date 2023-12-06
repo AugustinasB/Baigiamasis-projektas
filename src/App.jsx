d16edae8b8437542d195fedffa036d4a63b5f112
AugustinasB <augustinasbaitis@gmail.com>
2023-12-06 11:19:20 +0200

    Pakeistas logo, stilius, pridetas teksto laukas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/header/Header';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearch = () => {
+    // Implement your search logic here using the searchTerm
+    console.log(`Searching for: ${searchTerm}`);
+  };
+
   return (
     <div>
-      <Header />
+      <Header
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={handleInputChange}
+      />
     </div>
   );
 }
